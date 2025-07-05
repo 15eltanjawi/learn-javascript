824f821873d448bd257ab7cab89f584196cd68ad
15eltanjawi <167198263+15eltanjawi@users.noreply.github.com>
2025-07-05 11:43:53 +0100

    Update index.js
    this is my solution to this project

--- 2. Practice time - part 1/01. Variables practice/index.js
+++ 2. Practice time - part 1/01. Variables practice/index.js
@@ -1,7 +1,8 @@
 // Create two variables, firstName and lastName
-
+let firstName = 'aziz';
+let lastName = 'boudarbala';
 // Concatenate the two variables into a third variable called fullName
-
+let fullName = firstName + lastName ;
 // Log fullName to the console
-
+console.log(fullName);
 
